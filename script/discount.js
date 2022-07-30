535395013d3f77615eb27adf0bb521bb1cdc03c2
RennanPeron <rennanperon01@gmail.com>
2022-07-30 18:33:51 -0300

    discount script fix

--- script/discount.js
+++ script/discount.js
@@ -2,7 +2,7 @@ let productItem = {
     name: "Fall Limited Edition Sneakers",
     price: "250.00",
     hasDiscount: true,
-    discountValue: "50.00",
+    discountValue: "50",
     discountedPrice: "125.00"
 }
 
@@ -13,7 +13,20 @@ let applyDiscount = () => {
     
     if(productItem.hasDiscount) {
         price.innerHTML = productItem.discountedPrice
-        
+        // VALOR EM PORCENTAGEM DO DESCONTO
+        var discount = document.createElement("p")
+        discount.setAttribute("id", "discount")
+        var percent = document.createTextNode(productItem.discountValue + "%")
+        discount.appendChild(percent)
+        var element = document.getElementById("price-value");
+        element.appendChild(discount);
+        // VALOR DO PREÇO ORIGINAL
+        var discountedPrice = document.createElement("p")
+        discountedPrice.setAttribute("id", "originalPrice")
+        var value = document.createTextNode("$" + productItem.price)
+        discountedPrice.appendChild(value)
+        var element = document.getElementById("price-info");
+        element.appendChild(discountedPrice);
     }
     
 }
