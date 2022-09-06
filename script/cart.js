4fe46649c3d63ed0c1be52662217304c55b0d427
RennanPeron <rennanperon01@gmail.com>
2022-09-06 15:35:50 -0300

    added cart script

--- script/cart.js
+++ script/cart.js
@@ -1,19 +1,76 @@
-const addToCart = () => {
-    // var price = document.getElementById("price")
+const addToCart = (item = productItem ) => {
+    // ELEMENTOS HTML
     var count = document.getElementById("cart-count")
     var quantity = document.querySelector(".cart-add")
-    // CRIAR UM ARRAY DE PRODUTOS
+    var checkout = document.querySelector(".cart-item--checkout")
+    var emptyCart = document.querySelector(".cart-items--empty")
 
-    // SOMAR O PREÇO TOTAL DOS PRODUTOS
+    if (parseInt(quantity.innerHTML) != 0){
+        // CRIAR OS ELEMENTOS 
+        // DIV
+        const cartItemProduct = document.createElement("div")
+        const cartItemInfo = document.createElement("div")
+        // P
+        const cartItemTitle = document.createElement("h3")
+        const cartItemValue = document.createElement("p")
+        const deleteCartItem = document.createElement("p")
+        const multiples = document.createElement("span")
+        // IMG
+        const cartItemImg = document.createElement("img")
+        cartItemImg.src = item.image
 
-    // CONTAR A QUANTIDADE DE PRODUTOS
+        //  ADICIONAR AS CLASSES 
+        cartItemProduct.classList.add("cart-item--product")
+        cartItemInfo.classList.add("cart-item--info")
+        cartItemValue.classList.add("cart-item-value")
+        deleteCartItem.classList.add("delete-cart-item")
+        cartItemImg.classList.add("cart-item-img")
+        multiples.classList.add("multiples")
 
-    // ADICIONAR ESSA QUANTIDADE AO CARRINHO
-    count.innerHTML = quantity.innerHTML
+        // VALORES DO PRODUTO - innerText? 
+        if(item.hasDiscount) {
+            cartItemValue.innerHTML =  "$" + item.discountedPrice
+        } else {
+            cartItemValue.innerHTML = "$" + item.price
+        }
+        multiples.setAttribute('data', quantity.innerHTML)
+        multiples.innerHTML = ` x${quantity.innerHTML} $${(quantity.innerHTML * Number(cartItemValue.innerHTML.split("$")[1]))}.00 `
+
+        cartItemTitle.innerHTML = item.name
+        
+        // CONFIGURANDO A HIERARQUIA
+        cartItemValue.appendChild(multiples)
+
+        cartItemInfo.appendChild(cartItemTitle)
+        cartItemInfo.appendChild(cartItemValue)
+
+        cartItemProduct.appendChild(cartItemImg)
+        cartItemProduct.appendChild(cartItemInfo)
+        cartItemProduct.appendChild(deleteCartItem)
+
+        // ADICIONANDO O PRODUTO AO CARRINHO
+        document.querySelector(".cart-items").appendChild(cartItemProduct)
+        
+        deleteCartItem.addEventListener("click", () => {
+            document.querySelector(".cart-items").removeChild(deleteCartItem.parentNode)
+            count.innerHTML = count.innerHTML - multiples.getAttribute("data")
+            checkCount(count, checkout, emptyCart)
+        })
+        }
+    // ADICIONAR A QUANTIDADE AO CARRINHO
+    count.innerHTML = parseInt(quantity.innerHTML) + parseInt(count.innerHTML)
+    checkCount(count, checkout, emptyCart)
+}
+
+const checkCount = (count, checkout, emptyCart) => {
     if (parseInt(count.innerHTML) == 0) {
         count.setAttribute("class", "no-count")
-    } else {
+        checkout.classList.add("hide")
+        emptyCart.classList.remove("hide")
+    } else {    
         count.removeAttribute("class")
+        checkout.classList.remove("hide")
+        emptyCart.classList.add("hide")
     }
 }
 
@@ -33,4 +90,8 @@ const quantityAdd = (btn) => {
 
 const openCart = () => {
     document.querySelector("#cart-box").classList.toggle("close")
+}
+
+const closeCart = () => {
+    document.querySelector("#cart-box").classList.add("close")
 }
--- script/discount.js
+++ script/discount.js
@@ -3,28 +3,28 @@ let productItem = {
     price: "250.00",
     hasDiscount: true,
     discountValue: "50",
-    discountedPrice: "125.00"
+    discountedPrice: "125.00",
+    image: "../img/image-product-1-thumbnail.jpg"
 }
 
-let applyDiscount = () => {
+let applyDiscount = (item = productItem) => {
     let price = document.getElementById("price")
     
-    if(productItem.hasDiscount) {
-        price.innerHTML = "$" + productItem.discountedPrice
+    if(item.hasDiscount) {
+        price.innerHTML = "$" + item.discountedPrice
         // VALOR EM PORCENTAGEM DO DESCONTO
         var discount = document.createElement("p")
         discount.setAttribute("id", "discount")
-        var percent = document.createTextNode(productItem.discountValue + "%")
+        var percent = document.createTextNode(item.discountValue + "%")
         discount.appendChild(percent)
         var element = document.getElementById("price-value");
         element.appendChild(discount);
         // VALOR DO PREÇO ORIGINAL
         var discountedPrice = document.createElement("p")
         discountedPrice.setAttribute("id", "originalPrice")
-        var value = document.createTextNode("$" + productItem.price)
+        var value = document.createTextNode("$" + item.price)
         discountedPrice.appendChild(value)
         var element = document.getElementById("price-info");
         element.appendChild(discountedPrice);
     }
-    
 }
